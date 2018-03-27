0ef3941e3a539f3e319c9d7815d86ee540103d91
Xander Madsen <xander.madsen@gmail.com>
2018-03-27 13:48:20 -0400

    Add Tile to Board initialization, make Tile 0 by default

--- functional_tests.py
+++ functional_tests.py
@@ -7,18 +7,28 @@ class BoardAndTileTest(unittest.TestCase):
     def setUp(self):
         self.board = threesgame.Board()
 
-    def test_board_returns_4_by_4_board_and_array_of_possible_next_tiles(self):
+    def test_board_read_board_method_returns_4_by_4_list_and_array_of_possible_tiles(self):
         # I go to make a board.
         boardoutput = self.board.read_board()
 
         # It contains four arrays of length four,
-        self.assertEquals(len(boardoutput[0]), 4)
-        self.assertEquals(len(boardoutput[1]), 4)
-        self.assertEquals(len(boardoutput[2]), 4)
-        self.assertEquals(len(boardoutput[3]), 4)
+        tiles = boardoutput["tiles"]
+        for row in tiles:
+            self.assertEqual(len(row), 4)
 
         # and one array of length 1 to 3 to indicate the next possible tile(s)
-        self.assertIn(len(boardoutput[4]), [1, 2, 3])
+        next_tiles = boardoutput["next_tiles"]
+        self.assertIn(len(next_tiles), [1, 2, 3])
+
+    def test_board_tiles_returns_a_4_by_4_list_of_tile_objects(self):
+        # The board contains four arrays, where each element is a Tile object
+        # I go to make a board.
+        boardoutput = self.board.read_board()
+        tiles = boardoutput["tiles"]
+        # It contains four arrays four Tiles each,
+        for row in tiles:
+            for element in row:
+                self.assertIsInstance(element, threesgame.Tile)
 
 
 if __name__ == '__main__':
--- threesgame.py
+++ threesgame.py
@@ -5,7 +5,22 @@ A clone of the iOS game Threes, to be used in training a neural network.
 """
 
 
+class Tile():
+    value = 0
+
+    def __str__(self):
+        return(str(self.value))
+
+
 class Board():
-    @staticmethod
-    def read_board():
-        return([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [1, 2, 3]])
+
+    tiles = [[Tile(), Tile(), Tile(), Tile()],
+             [Tile(), Tile(), Tile(), Tile()],
+             [Tile(), Tile(), Tile(), Tile()],
+             [Tile(), Tile(), Tile(), Tile()]]
+
+    def read_board(self):
+        output = {}
+        output["tiles"] = self.tiles
+        output["next_tiles"] = [1, 2, 3]
+        return(output)
