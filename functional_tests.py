164fe680a3451ee63bb23a38b81b1f85a3ee617e
Xander Madsen <xander.madsen@gmail.com>
2018-03-27 16:00:34 -0400

    Add board_init() to set up initial board state (3x each of 1, 2, and 3 tiles)

--- functional_tests.py
+++ functional_tests.py
@@ -12,7 +12,7 @@ class BoardAndTileTest(unittest.TestCase):
         boardoutput = self.board.read_board()
 
         # It contains four arrays of length four,
-        tiles = boardoutput["tiles"]
+        tiles = boardoutput["tiles_layout"]
         for row in tiles:
             self.assertEqual(len(row), 4)
 
@@ -20,15 +20,38 @@ class BoardAndTileTest(unittest.TestCase):
         next_tiles = boardoutput["next_tiles"]
         self.assertIn(len(next_tiles), [1, 2, 3])
 
-    def test_board_tiles_returns_a_4_by_4_list_of_tile_objects(self):
+    def test_board_read_board_method_returns_a_4_by_4_list_of_integers_and_array_of_integers(self):
         # The board contains four arrays, where each element is a Tile object
         # I go to make a board.
         boardoutput = self.board.read_board()
-        tiles = boardoutput["tiles"]
-        # It contains four arrays four Tiles each,
+        tiles = boardoutput["tiles_layout"]
+
+        # It contains four arrays with four Tiles each,
         for row in tiles:
             for element in row:
-                self.assertIsInstance(element, threesgame.Tile)
+                self.assertIsInstance(element, int)
+
+        next_tiles = boardoutput["next_tiles"]
+
+        for number in next_tiles:
+            self.assertIsInstance(number, int)
+
+    def test_initialized_board_has_three_of_each_starting_tile(self):
+        self.board.board_init()
+
+        boardoutput = self.board.read_board()
+        ones = twos = threes = zeros = 0
+        for row in boardoutput["tiles_layout"]:
+            ones += row.count(1)
+            twos += row.count(2)
+            threes += row.count(3)
+            zeros += row.count(0)
+
+        # There are three 1s, three 2s, and three 3s to start.
+        self.assertEqual(ones, 3)
+        self.assertEqual(twos, 3)
+        self.assertEqual(threes, 3)
+        self.assertEqual(zeros, 7)
 
 
 if __name__ == '__main__':
--- threesgame.py
+++ threesgame.py
@@ -4,6 +4,8 @@ Xander Madsen - March 2018
 A clone of the iOS game Threes, to be used in training a neural network.
 """
 
+from random import randint
+
 
 class Tile():
     value = 0
@@ -21,6 +23,22 @@ class Board():
 
     def read_board(self):
         output = {}
-        output["tiles"] = self.tiles
+        output["tiles_layout"] = [[el.value for el in row]
+                                  for row in self.tiles]
         output["next_tiles"] = [1, 2, 3]
         return(output)
+
+    def board_init(self):
+        coords_to_change = []
+
+        # Pick 9 tiles to be initialized with nonzero values
+        while len(coords_to_change) < 9:
+            coord = (randint(0, 3), randint(0, 3))
+            if coord not in coords_to_change:
+                coords_to_change.append(coord)
+
+        initial_values = [1, 1, 1, 2, 2, 2, 3, 3, 3]
+
+        for coord in coords_to_change:
+            self.tiles[coord[0]][coord[1]].value = initial_values.pop(
+                randint(0, len(initial_values) - 1))
